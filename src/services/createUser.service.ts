dd4ac76edd4612b50a3e5283a26d2bc1d863949a
Edeno Scherer <edenoscherer@gmail.com>
2024-11-02 16:03:08 -0300

    feat: :package: criptografa a senha ao criar um usuário

--- src/services/createUser.service.ts
+++ src/services/createUser.service.ts
@@ -1,11 +1,18 @@
 import { User } from '../entities/user';
 import { CreateUserRepository } from '../repositories/users/interfaces/createUser.repository';
+import { EncryptionService } from './encryption.service';
 
 export class CreateUserService {
   constructor(private readonly createUserRepository: CreateUserRepository) {}
 
   async create(user: User): Promise<User> {
-    const id = await this.createUserRepository.createUser(user);
+    let { password } = user;
+    if (!password?.trim().length) {
+      throw new Error('Password is required and cannot be blank');
+    }
+    password = await EncryptionService.hash(password.trim());
+
+    const id = await this.createUserRepository.createUser({ ...user, password });
 
     return { ...user, id };
   }
