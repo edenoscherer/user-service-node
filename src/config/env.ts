2cc98ed48a57d4de9f4b125d74370936b059af94
Edeno Scherer <edenoscherer@gmail.com>
2024-11-04 14:28:15 -0300

    fix: :bug: correção aplicada ao nome das variáveis ​​de ambiente

--- src/config/env.ts
+++ src/config/env.ts
@@ -10,11 +10,11 @@ dotenv.config({ path: envFile });
 export const ENV = {
   NODE_ENV: process.env.NODE_ENV ?? 'dev',
   PORT: process.env.PORT?.length ? parseInt(process.env.PORT) : 3000,
-  DB_HOST: process.env.DB_HOST ?? 'localhost',
-  DB_USER: process.env.DB_USER ?? 'user_service',
-  DB_PASSWORD: process.env.DB_PASSWORD ?? 'user@service',
-  DB_NAME: process.env.DB_NAME ?? 'user_service',
-  DB_PORT: process.env.DB_PORT?.length ? parseInt(process.env.DB_PORT) : 3306,
+  DB_HOST: process.env.MYSQL_HOST ?? 'localhost',
+  DB_USER: process.env.MYSQL_USER ?? 'user_service',
+  DB_PASSWORD: process.env.MYSQL_PASSWORD ?? 'user@service',
+  DB_NAME: process.env.MYSQL_DATABASE ?? 'user_service',
+  DB_PORT: process.env.MYSQL_PORT?.length ? parseInt(process.env.MYSQL_PORT) : 3306,
   JWT_SECRET: process.env.JWT_SECRET ?? 'secret',
   JWT_EXPIRATION: process.env.JWT_EXPIRATION ?? '15m'
 };
