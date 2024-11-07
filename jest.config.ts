87922c661ed813a7d3198d317ef74288f4a51998
Edeno Scherer <edenoscherer@gmail.com>
2024-11-06 22:58:28 -0300

    feat: :sparkles: update Jest configuration and add custom paths

--- jest.config.ts
+++ jest.config.ts
@@ -1,17 +1,20 @@
-import type {Config} from 'jest';
+import type { Config } from 'jest';
 import { createDefaultPreset } from 'ts-jest';
 
-const defaultPreset = createDefaultPreset()
+const defaultPreset = createDefaultPreset();
 const config: Config = {
-  roots: ["<rootDir>/src", "<rootDir>/tests"],
-  collectCoverageFrom: ["<rootDir>/src/**/*.ts"],
+  roots: ['<rootDir>/src', '<rootDir>/tests'],
+  collectCoverageFrom: ['<rootDir>/src/**/*.ts'],
   clearMocks: true,
   collectCoverage: true,
-  coverageDirectory: "coverage",
-  coverageProvider: "v8",
-  testEnvironment: "node",
-  setupFilesAfterEnv: ["<rootDir>/tests/jest.setup.ts"],
-  ...defaultPreset
+  coverageDirectory: 'coverage',
+  coverageProvider: 'v8',
+  testEnvironment: 'node',
+  setupFilesAfterEnv: ['<rootDir>/tests/jest.setup.ts'],
+  moduleNameMapper: {
+    '^@/(.*)$': '<rootDir>/src/$1',
+  },
+  ...defaultPreset,
 };
 
 export default config;
